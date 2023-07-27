44df5d5321a9ebf3f671abd393bfc6fb64ff692f
Kajal Pandey <119345768+sp8172975@users.noreply.github.com>
2023-07-27 10:11:24 +0530

    Completed

---
+++ my-app/src/Form.js
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from 'react'
+
+function Form () {
+  const data = {name:"", email:"", password:""};
+  const[inputData, setInputData] = useState(data)
+  const[flag, setFlag] = useState(false)
+  useEffect(() =>{
+   console.log("Registered")
+  }, [flag])
+
+  function handleData(e){
+    setInputData({...inputData, [e.target.name]:e.target.value})
+    console.log(inputData)
+  }
+
+  function handleSubmit(e){
+    e.preventDefault();
+    if(!inputData.name || !inputData.email || !inputData.password){
+      alert("All fields are Mandatory")
+    
+  }
+  else{
+    setFlag(true)
+
+  }
+  }
+  return (
+    <>
+    <pre>{(flag)?<h2 className='ui-define'>Hello {inputData.name}, You've Registered Successfully</h2> :""}</pre>
+    <form className='container' onSubmit={handleSubmit}>
+    <div className='header'>
+        <h1>Registration Form</h1>
+    </div>
+    <div>
+      <input type='text' placeholder="Enter Your Name"
+      name="name" value={inputData.name} onChange={handleData}></input>
+    </div>
+    <div>
+      <input type='text' placeholder="Enter Your Email"
+      name="email" value={inputData.email} onChange={handleData}></input>
+    </div>
+    <div>
+      <input type='text' placeholder="Enter Your Password"
+      name="password" value={inputData.password} onChange={handleData}></input>
+    </div>
+    <div>
+    <button type='submit'>Submit</button>
+    </div>
+  </form>
+    
+    </>
+  
+  );
+}
+
+export default Form
